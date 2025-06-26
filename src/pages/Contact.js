b68d7637747c81a5adb34ba20ef38d539a463084
KilianBrd <kilianberaud3@gmail.com>
2025-06-26 18:38:31 +0200

    - Remplacement bootstrap -> tailwind.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,9 @@
 function Contact() {
   return (
     <div>
-      <h1>Contact</h1>
+      <h1 className="text-3xl font-bold underline text-center mt-5 mb-5 text-gr">
+        Contact
+      </h1>
     </div>
   );
 }
